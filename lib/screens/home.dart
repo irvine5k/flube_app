27bdae3b58e415a99ff474df597e4c2a1cbb7dc5
Kelven <kelvengalvao@gmail.com>
2019-04-12 23:52:13 -0300

    Home AppBar.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,42 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Container(
+              height: 40,
+              child: Image.asset("images/logo.png"),
+            ),
+            SizedBox(width: 6),
+            Text(
+              'Flube',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
